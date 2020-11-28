04ed4c3f862a50cad1dd1daebfb21f2380277d67
alvinfrancis <alvin.francis.dumalus@gmail.com>
2020-11-28 21:55:43 +0100

    Disable use of +tabs/display for elscreen

--- modules/feature/tabs/autoload/tabs.el
+++ modules/feature/tabs/autoload/tabs.el
@@ -1,7 +1,5 @@
 ;;; feature/tabs/autoload/workspaces.el -*- lexical-binding: t; -*-
 
-(defalias #'+tabs/display #'elscreen-toggle-display-tab)
-
 ;;;###autoload
 (defun +tabs/new ()
   (interactive)
