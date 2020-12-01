8a8377733d8809bad710f875ffeff47a7ab8687c
alvinfrancis <alvin.francis.dumalus@gmail.com>
2020-12-01 20:31:11 +0100

    First pass use of tab-bar in Emacs 27

--- modules/feature/tabs/config.el
+++ modules/feature/tabs/config.el
@@ -1,27 +1,52 @@
 ;;; feature/tabs/config.el -*- lexical-binding: t; -*-
 
-(def-package! elscreen
+(def-package! tab-bar
   :init
-  (setq elscreen-tab-display-control nil
-        elscreen-display-tab nil
-        elscreen-display-screen-number nil
-        elscreen-tab-display-kill-screen nil)
+  (defun +tab-bar/tab-name ()
+    (let* ((buffer-name (concat " "
+                                (buffer-name (window-buffer (minibuffer-selected-window)))
+                                " ")))
+      (concat
+       (+doom-modeline--make-xpm
+        (face-background 'doom-modeline-inactive-bar nil t)
+        +doom-modeline-height
+        +doom-modeline-bar-width)
+       buffer-name)))
 
-  ;; (defun +elscreen/quit ()
-  ;;   (interactive)
-  ;;   (if (and (not (elscreen-one-screen-p))
-  ;;            (one-window-p))
-  ;;       (elscreen-kill)
-  ;;     (evil-quit)))
-  ;; (evil-ex-define-cmd "quit" #'+elscreen/quit)
+  (setq tab-bar-close-button-show nil
+        tab-bar-new-button-show nil
+        tab-bar-show 1
+        tab-bar-tab-name-function #'+tab-bar/tab-name)
 
   :config
-  (defalias #'+tabs/display #'elscreen-toggle-display-tab)
-  (defalias #'+tabs/new #'elscreen-create)
-  (defalias #'+tabs/close #'elscreen-kill)
-  (defalias #'+tabs/switch-left #'elscreen-previous)
-  (defalias #'+tabs/switch-right #'elscreen-next)
-  (elscreen-start))
+  (defalias #'+tabs/display #'toggle-tab-bar-mode-from-frame)
+  (defalias #'+tabs/new #'tab-bar-new-tab)
+  (defalias #'+tabs/close #'tab-bar-close-tab)
+  (defalias #'+tabs/switch-left #'tab-bar-switch-to-prev-tab)
+  (defalias #'+tabs/switch-right #'tab-bar-switch-to-next-tab))
+
+;; (def-package! elscreen
+;;   :init
+;;   (setq elscreen-tab-display-control nil
+;;         elscreen-display-tab nil
+;;         elscreen-display-screen-number nil
+;;         elscreen-tab-display-kill-screen nil)
+
+;;   ;; (defun +elscreen/quit ()
+;;   ;;   (interactive)
+;;   ;;   (if (and (not (elscreen-one-screen-p))
+;;   ;;            (one-window-p))
+;;   ;;       (elscreen-kill)
+;;   ;;     (evil-quit)))
+;;   ;; (evil-ex-define-cmd "quit" #'+elscreen/quit)
+
+;;   :config
+;;   (defalias #'+tabs/display #'elscreen-toggle-display-tab)
+;;   (defalias #'+tabs/new #'elscreen-create)
+;;   (defalias #'+tabs/close #'elscreen-kill)
+;;   (defalias #'+tabs/switch-left #'elscreen-previous)
+;;   (defalias #'+tabs/switch-right #'elscreen-next)
+;;   (elscreen-start))
 
 ;; (def-package! centaur-tabs
 ;;   :init
@@ -37,8 +62,8 @@
 ;;         centaur-tabs-cycle-scope 'tabs
 ;;         centaur-tabs--buffer-show-groups t
 ;;         )
-;;   (defalias #'+tabs/display #'centaur-tabs-local-mode)
-;;   (defalias #'+tabs/switch-left #'centaur-tabs-backward)
-;;   (defalias #'+tabs/switch-right #'centaur-tabs-forward)
+;;   ;; (defalias #'+tabs/display #'centaur-tabs-local-mode)
+;;   ;; (defalias #'+tabs/switch-left #'centaur-tabs-backward)
+;;   ;; (defalias #'+tabs/switch-right #'centaur-tabs-forward)
 ;;   :config
 ;;   (centaur-tabs-mode t))
