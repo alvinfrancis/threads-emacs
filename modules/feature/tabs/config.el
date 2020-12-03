76bbe45c445eac63e891a8269de5fffead2a072c
alvinfrancis <alvin.francis.dumalus@gmail.com>
2020-12-03 17:02:22 +0100

    Tab-bar improvements

--- modules/feature/tabs/config.el
+++ modules/feature/tabs/config.el
@@ -7,23 +7,30 @@
                                 (buffer-name (window-buffer (minibuffer-selected-window)))
                                 " ")))
       (concat
-       (+doom-modeline--make-xpm
-        (face-background 'doom-modeline-inactive-bar nil t)
-        +doom-modeline-height
-        +doom-modeline-bar-width)
+       (when (display-graphic-p)
+         (+doom-modeline--make-xpm
+          ;; (face-background (if (window-buffer (minibuffer-selected-window))
+          ;;                      'doom-modeline-bar
+          ;;                    'doom-modeline-inactive-bar) nil t)
+          nil
+          +doom-modeline-height
+          +doom-modeline-bar-width))
        buffer-name)))
 
   (setq tab-bar-close-button-show nil
         tab-bar-new-button-show nil
         tab-bar-show 1
-        tab-bar-tab-name-function #'+tab-bar/tab-name)
+        tab-bar-tab-name-function #'+tab-bar/tab-name
+        tab-bar-separator ""
+        tab-bar-new-tab-to 'rightmost)
 
   :config
   (defalias #'+tabs/display #'toggle-tab-bar-mode-from-frame)
   (defalias #'+tabs/new #'tab-bar-new-tab)
   (defalias #'+tabs/close #'tab-bar-close-tab)
   (defalias #'+tabs/switch-left #'tab-bar-switch-to-prev-tab)
-  (defalias #'+tabs/switch-right #'tab-bar-switch-to-next-tab))
+  (defalias #'+tabs/switch-right #'tab-bar-switch-to-next-tab)
+  (tab-bar-mode t))
 
 ;; (def-package! elscreen
 ;;   :init
