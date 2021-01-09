52d977bbc9a48dbf00717a412fbcf8375bf3f4af
alvinfrancis <alvin.francis.dumalus@gmail.com>
2021-01-09 17:11:46 +0100

    More clojure module updates

--- modules/lang/clojure/config.el
+++ modules/lang/clojure/config.el
@@ -1,6 +1,8 @@
 ;;; lang/clojure/config.el -*- lexical-binding: t; -*-
 
-(def-package! flycheck-clj-kondo)
+(def-package! flycheck-clj-kondo
+  :when (featurep! :checkers syntax)
+  :after flycheck)
 
 (def-package! clojure-mode
   :mode "\\.clj$"
@@ -13,22 +15,32 @@
         [remap eval-region]          #'cider-eval-region
         [remap eval-buffer]          #'cider-eval-buffer
         (:localleader
-          :n "c" #'cider-jack-in
-          :n "C" #'cider-connect
-          :n "z" #'cider-switch-to-repl-buffer))
-  (map! :map cider-repl-mode-map
-        (:localleader
-          :n "z" #'cider-switch-to-last-clojure-buffer)))
+          (:desc "CLJ" :prefix "c"
+            :n "j" #'cider-jack-in-clj
+            :n "c" #'cider-connect-clj)
+          (:desc "CLJS" :prefix "C"
+            :n "j" #'cider-jack-in-cljs
+            :n "c" #'cider-connect-cljs))))
 
 
 (def-package! cider
   ;; NOTE: if you don't have an org directory set (the dir doesn't exist), cider jack in won't work.
   :commands (cider-jack-in cider-mode cider-jack-in-clojurescript)
   :config
-  (setq nrepl-hide-special-buffers t)
+  (setq nrepl-hide-special-buffers t
+        cider-repl-display-help-banner nil
+        cider-repl-pop-to-buffer-on-connect 'display-only)
 
   (set! :jump 'cider-mode
     :definition #'cider-find-var)
+
+  (map! :map cider-mode-map
+        (:localleader
+          :n "z" #'cider-switch-to-repl-buffer))
+
+  (map! :map cider-repl-mode-map
+        (:localleader
+          :n "z" #'cider-switch-to-last-clojure-buffer))
 
   ;; settings for cider repl as a popup (prevent it from being closed on escape, especially.)
   (set! :popup "^\\*cider" :regexp t :noselect t :noesc t))
