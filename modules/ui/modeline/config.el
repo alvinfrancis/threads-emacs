6d47c53a04db7f53d13f33a6c2eb6fbc0910fae9
alvinfrancis <alvin.francis.dumalus@gmail.com>
2021-03-11 13:54:16 +0100

    Use doom-modeline

---
+++ modules/ui/modeline/config.el
@@ -0,0 +1,5 @@
+;;; ui/modeline/config.el -*- lexical-binding: t; -*-
+
+(def-package! doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode))
---
+++ modules/ui/modeline/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; ui/modeline/packages.el
+
+(package! doom-modeline)
