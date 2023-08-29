f1767f10a9faeb92b2404348149f542f72822ffd
alvinfrancis <alvin.francis.dumalus@gmail.com>
2023-08-29 18:27:08 +0200

    Add terraform

---
+++ modules/lang/terraform/config.el
@@ -0,0 +1,4 @@
+;;; lang/terraform/config.el -*- lexical-binding: t; -*-
+
+(def-package! terraform-mode
+  :mode "\\.tf$")
---
+++ modules/lang/terraform/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/terraform/packages.el
+
+(package! terraform-mode)
