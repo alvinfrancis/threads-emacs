d0d3f42a556035c310a9c73bd42b56d120381f02
alvinfrancis <alvin.francis.dumalus@gmail.com>
2025-02-28 01:21:13 +0100

    Add copilot to AI tools

--- modules/tools/ai/config.el
+++ modules/tools/ai/config.el
@@ -8,3 +8,11 @@
 
 (def-package! gptel-transient
   :commands (gptel-menu))
+
+(def-package! copilot
+ :hook (prog-mode . copilot-mode)
+ :bind (:map copilot-completion-map
+             ("<tab>" . 'copilot-accept-completion)
+             ("TAB" . 'copilot-accept-completion)
+             ("C-TAB" . 'copilot-accept-completion-by-word)
+             ("C-<tab>" . 'copilot-accept-completion-by-word)))
--- modules/tools/ai/packages.el
+++ modules/tools/ai/packages.el
@@ -3,7 +3,8 @@
 
 (package! gptel)
 
+(package! copilot
+  :recipe (:fetcher github :repo "copilot-emacs/copilot.el" :file ("*.el")))
 
 ;; Add to ~/.authinfo the following line
 ;; machine api.openai.com login apikey password TOKEN
-
